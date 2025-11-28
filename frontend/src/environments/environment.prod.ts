689f209cf38f9825950c26afd9268565dc6677f3
Oleg <oleggrom12@gmail.com>
2025-11-28 22:35:12 +0300

    Add node version

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3000/api/',
-  serverStaticPath: 'http://localhost:3000/images/products/'
+  api: 'https://backend-xtn4.onrender.com/api/',
+  serverStaticPath: 'https://backend-xtn4.onrender.com/images/products/'
 };
