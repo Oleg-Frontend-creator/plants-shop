c507aef80589457381102ebf4cc692751d4481d3
Oleg <oleggrom12@gmail.com>
2026-02-14 12:24:42 +0300

    Change environment variables for backend integration

--- backend/app.js
+++ backend/app.js
@@ -25,16 +25,18 @@ MongoDBConnection.getConnection((error, connection) => {
     }
     const app = express();
 
-    app.get('/test', (req, res) => {
-        res.json({message: 'Back is alive'});
-    });
-
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.json());
-    app.use(cors({
-        credentials: true,
-        origin: true
-    }));
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200);
+        }
+        next();
+    });
 
     app.use(session({
         genid: function (req) {
